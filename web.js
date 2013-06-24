e0aeab6e29835dc0c8452fb628cd82d64ef0e0fc
root <root@rekdeby>
2013-06-24 04:59:32 +0200

    modif2 web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var homepage=fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(homepage);
+  response.send(homepage.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
