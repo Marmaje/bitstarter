f5342077763a372960bf649d361b0492785a39ce
root <root@rekdeby>
2013-06-24 05:05:15 +0200

    nouveau test

--- web.js
+++ web.js
@@ -1,11 +1,9 @@
 var express = require('express');
-var fs = require('fs')
 var app = express.createServer(express.logger());
 
-var homepage=fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(homepage.toString('utf8'));
+  response.send('utf8');
 });
 
 var port = process.env.PORT || 5000;
